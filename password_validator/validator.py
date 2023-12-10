7a59ed1b76f25c6ad841d2bbb535486c0480bde9
juzor <chigozirim.uzor@dmu.ac.uk>
2023-12-10 14:20:23 +0000

    make tests pass

--- password_validator/validator.py
+++ password_validator/validator.py
@@ -3,5 +3,9 @@ class PasswordValidator:
     def validate_password(self, password):
         return (
             len(password) >= 8
+            and any(char.isupper() for char in password)
+            and any(char.islower() for char in password)
+            and any(char.isdigit() for char in password)
+            and "_" in password
         )
             
